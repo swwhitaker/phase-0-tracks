330893bb9b7cb57da429868ab791e3f0c8e46fe6
“Stephen <s.w.whitaker@icloud.com>
2016-08-18 11:36:05 -0400

    Design and create puppy class for 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+##Pseudocode a class design for Puppy class. 6.1 release 0
+# SPECIES ------------------
+# Puppy
+
+# CHARACTERISTICS ----------
+# Cuteness: extreme
+# Friendly: very
+# Nose: wet
+# Tail: waggly
+# Name: varies
+# Color: varies
+
+# Behavior -----------------
+# Bark
+# Cuddle
+# Eat
+# Fetch
+
+
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
