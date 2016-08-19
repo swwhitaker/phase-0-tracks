e28ef553bb8f4c1e5f62a3a572d831d95563a7ce
“Stephen <s.w.whitaker@icloud.com>
2016-08-19 19:35:55 -0400

    complete 6.6; paired with Aidan Legaspi

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,3 +1,26 @@
+class TodoList
+  def initialize(list)
+    @list = list
+  end
+
+  def get_items
+    @list 
+  end
+
+  def add_item(item)
+    @list << item
+  end
+
+  def delete_item(item)
+    @list.delete(item)
+  end
+
+  def get_item(index)
+    @list[index]
+  end
+  
+end
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
