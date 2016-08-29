e28d645d7b379f5730f1b882f785bbc1a94f0a20
“Stephen <s.w.whitaker@icloud.com>
2016-08-29 10:23:37 -0400

    add string generator for use inother functions; first crack at object comparison schema)

---
+++ js/algos.js
@@ -0,0 +1,33 @@
+
+function generate_string() {
+	var randstring = Math.random().toString(36).substring(20);
+	randstring.length = Math.random(20);
+	console.log(randstring.length);
+	return randstring;
+}
+
+generate_string()
+
+
+function longest_word(array){
+	
+}
+
+
+longest_word(["long phrase","longest phrase","longer phrase"])
+
+
+
+
+// write a function that takes two objects and checks to see if they share at least one key-value pair
+// input - two objects
+// make loop call the data for each object at each key, and each value
+// create conditional statement to set to true if key is same, and another for if value is same
+// create another conditional statement to make test boolean true if key: same && value: same
+
+var object1 = {name: "Steven", age: 54}
+var object2 = {name: "Tamir", age: 54}
+
+function compare(object1, object2){
+	if object1[name:]
+}
